3bcb32a51fb0241ddb32e6bd169b949b201523ef
Arpit Patel <arpitpatel1771@gmail.com>
2025-02-04 23:47:35 +0530

    eod

--- 6-1.js
+++ 6-1.js
@@ -1,12 +1,37 @@
-class MapCellTypes {
-
+const MapCellTypes = {
+    empty: 1,
+    obstacle: 2,
 }
 
 class MapCell {
-    constructor(x, y, type) {
-        this.x = x
-        this.y = y
+    constructor(type) {
         this.type = type
         this.visitedByPlayer = false
     }
 }
+
+class Map {
+    constructor(stringInput) {
+        this.map = []
+
+        stringInput.split("\n").forEach(row => {
+            const cells = []
+            row.split("").forEach(cell => {
+                let type = undefined
+                if (cell == ".") {
+                    type = MapCellTypes.empty
+                } else if (cell == "#") {
+                    type = MapCellTypes.obstacle
+                } else if (["^", ">", "<", "V"].includes(cell)) {
+                    type = MapCellTypes.empty
+                } else {
+                    throw new Error("wtf")
+                }
+                const mapCell = new MapCell(MapCellTypes.empty)
+                cell.push(mapCell)
+            })
+        })
+
+    }
+}
+
